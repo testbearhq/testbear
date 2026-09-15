cab50a47b541cd46f9a58ff9a82ce05cf3d220f6
Manikandan P <64264387+manirolo@users.noreply.github.com>
2026-09-15 08:58:32 +0530

    chore: fix suppress warning

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
     >
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
